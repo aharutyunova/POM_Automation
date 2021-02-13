97e6d97fc75b4d6d67c04cf4530beb8a7afbecd7
Armine <arminestepanyan996@gmail.com>
2021-02-13 21:29:36 +0400

    save

--- SeleniumEasy_Anna/testdata.py
+++ SeleniumEasy_Anna/testdata.py
@@ -2,7 +2,7 @@ data={
     "url": "https://www.seleniumeasy.com/test/",
     "mainmenu": "Input Forms",
     "submenu": "Radio Buttons Demo",
-    "radiobutton": "Femalee"
+    "radiobutton": "Female"
     
 }
 
